f1b58e08c43fa40e3f04fd737e4ca36f021547a8
yonadav <yonadav.barilan@gmail.com>
2021-07-09 17:26:19 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,60 @@
 // safely handles circular references
-export const replacer = (cache:Array<any>|null) => (_key:string, value:any) =>
-  typeof value === "object" && value !== null
-    // @ts-ignore
-    ? cache.includes(value)
-    ? undefined // Duplicate reference found, discard key
-    // @ts-ignore
-    : cache.push(value) && value // Store value in our collection
-    : value;
+export const replacer = () => {
+  const cache:Array<any>|null = [];
 
-const withStringify = (call:(cache:Array<any>, obj:any, ...args:any[])=>string) => {
+  return (_key: string, value: any) =>
+    typeof value === "object" && value !== null
+      // @ts-ignore
+      ? cache.includes(value)
+        ? undefined // Duplicate reference found, discard key
+        // @ts-ignore
+        : cache.push(value) && value // Store value in our collection
+      : value;
+}
+
+export const replaceErrors = (_key: any, value: any) => {
+  if (value instanceof Error) {
+    const error: any = {};
+
+    Object.getOwnPropertyNames(value).forEach((propName) => {
+      error[propName] = value[propName];
+    });
+
+    return error;
+  }
+
+  return value;
+}
+
+const withStringify = (call:(obj:any, ...args:any[])=>string) => {
   return (obj:any, indent?: string | number) => {
     try {
-      let cache:Array<any>|null = [];
-      const retVal = call(cache, obj, indent);
-      cache = null;
-      return retVal;
+      return call(obj, indent);
     } catch (e) {
       return obj.toString()
     }
   }
 }
 
-export const stringify = withStringify((cache, obj):string => {
+export const stringify = withStringify((obj):string => {
   return JSON.stringify(
     obj,
-    replacer(cache),
+    replacer(),
   );
 });
 
-export const safeStringify = withStringify((cache, obj:any, indent = 2):string => {
+export const safeStringify = withStringify((obj:any, indent = 2):string => {
   return JSON.stringify(
     obj,
-    replacer(cache),
+    replacer(),
+    indent
+  );
+});
+
+export const stringifyError = withStringify((obj, indent):string => {
+  return JSON.stringify(
+    obj,
+    replacer(),
     indent
   );
 });
